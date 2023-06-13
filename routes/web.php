510aed8ecda2359b44d82ef0e585e1bccf2a92ec
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2023-06-13 20:47:29 +0200

    fix error

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/stringa', function(){
+    return "Sono una stringa";
+});
+
+Route::get('/intero', function(){
+    return 99;
+});
+
+Route::get('/array', function(){
+    $array=["Sono", "un", "array"];
+    return $array[2];
+});
+
+Route::get('/chi-siamo', function(){
+    return view('chisiamo');
+});
+
+Route::get('/contatti', function(){
+    return view('contatti');
+});
+
+Route::get('storia', function(){
+    return view('storia');
+});
